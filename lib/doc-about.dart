45bd3caac7383efdbff1610c58bea4ce784cac08
vynvanun <muhammmadgavyn@gmail.com>
2025-10-17 09:15:19 +0700

    Belajar fundamental flutter

---
+++ lib/doc-about.dart
@@ -0,0 +1,56 @@
+/**PENJELASAN */
+/**
+ * Stuktur Flutter terdiri dari
+ * 1. .dart_tool
+ * 2. .idea
+ * 3. android
+ *  => untuk konfigurasi android serta
+ *  => library android
+ * 4. build
+ *  =>hasil compile aplikasi
+ * 5. ios
+ *  => untuk konfigurasi ios serta
+ *  => library ios (native)
+ * 6. lib
+ *  => tempat file dart berada yang nantinya 
+ *  => sering dugunakan
+ * 7. linux
+ *  => untuk konfigurasi lunx
+ * 8. macos
+ *  => untuk konfigurasi macos
+ * 9. test
+ *  => tempat file untuk testinng aplikasi
+ * 10. web
+ *  => untuk konfigurasi web
+ * 11. windows
+ *  => untuk konfigurasi windows
+ * 12. pubspes.yaml
+ *  => tempat konfigurasi aplikasi
+ *  => menambahkan library
+ *  => menambahkan asset (gambar, fonr, dll)
+ * Di terminal 
+ * 
+ * kegunaan perintah fultter pub get
+ *  => untuk mensycron library yang ada di pubspec.yaml
+ * 
+ * * Kegunaan perintah "fultter run"
+ *  => menjalankan aplikasi
+ * 
+ * * Kegunaan perintah "flutter pub upgrade"
+ *  => mengupgrade library yang ada di pubspec.yaml
+ * 
+ * * Kegunaan perintah "flutter run"
+ *  => menjalankan aplikasi
+ * 
+ * *kegunaan perintah "flutter clean"
+ *  => membersihkan file build
+ * 
+ * library dari flutter bisa dilihat di pub.dev
+ * intl, http, button, dll
+ * 
+ * di dalamn dart ada 2 statement
+ * 1. statlest widget
+ *  => wideget yg tidak memiliki state(static)
+ * 2. stateful widget
+ *  => widget yg memiliki state (dynamic)
+ */
